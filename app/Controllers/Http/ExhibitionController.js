8cb153f3ebec9b9978ddc780f2b07271bfa4c037
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-18 16:25:27 -0300

    create endpoint for Exhibition

--- app/Controllers/Http/ExhibitionController.js
+++ app/Controllers/Http/ExhibitionController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Exhibition = use('App/Models/Exhibition')
+
 /**
  * Resourceful controller for interacting with exhibitions
  */
@@ -17,19 +19,10 @@ class ExhibitionController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index () {
+    const exhibition = await Exhibition.query().with('film').with('room').with('schedule').fetch()
 
-  /**
-   * Render a form to be used for creating a new exhibition.
-   * GET exhibitions/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return exhibition
   }
 
   /**
@@ -40,7 +33,11 @@ class ExhibitionController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request }) {
+    const data = request.only(['FilmId', 'RoomId', 'ScheduleId'])
+    const exhibition = Exhibition.create(data)
+
+    return exhibition
   }
 
   /**
@@ -52,19 +49,10 @@ class ExhibitionController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show ({ params }) {
+    const exhibition = await Exhibition.query().with('film').with('room').with('schedule').where('id', params.id).fetch()
 
-  /**
-   * Render a form to update an existing exhibition.
-   * GET exhibitions/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    return exhibition
   }
 
   /**
@@ -75,7 +63,14 @@ class ExhibitionController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request }) {
+    const exhibition = await Exhibition.findOrFail(params.id)
+    const data = request.only(['FilmId', 'RoomId', 'ScheduleId'])
+
+    exhibition.merge(data)
+    await exhibition.save()
+
+    return exhibition
   }
 
   /**
@@ -86,7 +81,12 @@ class ExhibitionController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params }) {
+    const exhibition = await Exhibition.findOrFail(params.id)
+
+    exhibition.IsActived = false
+
+    await exhibition.save()
   }
 }
 
