3dcc43f9b7102b5a0f623c7f8fb3196df210e62e
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-18 16:41:17 -0300

    ajust in Exhbition Model

--- app/Models/Exhibition.js
+++ app/Models/Exhibition.js
@@ -4,6 +4,17 @@
 const Model = use('Model')
 
 class Exhibition extends Model {
+    film(){
+        return this.belongsTo('App/Models/Film', 'FilmId', 'id')
+    }
+
+    room(){
+        return this.belongsTo('App/Models/Room', 'RoomId', 'id')
+    }
+
+    schedule(){
+        return this.belongsTo('App/Models/Schedule', 'ScheduleId', 'id')
+    }
 }
 
 module.exports = Exhibition
