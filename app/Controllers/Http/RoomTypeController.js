545841475f5ffe390d9e41cdaab13c44d13fd4b6
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-19 19:59:32 -0300

    add swagger documentation in room types

--- app/Controllers/Http/RoomTypeController.js
+++ app/Controllers/Http/RoomTypeController.js
@@ -1,93 +1,188 @@
-'use strict'
+"use strict";
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-const RoomType = use('App/Models/RoomType')
+const RoomType = use("App/Models/RoomType");
 
 /**
  * Resourceful controller for interacting with roomtypes
  */
 class RoomTypeController {
+  //#region [Swagger: Method GET]
   /**
-   * Show a list of all roomtypes.
-   * GET roomtypes
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @swagger
+   * /roomtypes:
+   *  get:
+   *    tags:
+   *      - Room Type
+   *    summary: Lists all room types
+   *    produces:
+   *      - application/json
+   *    responses:
+   *      200:
+   *        description: Return all room types
+   *        example:
+   *          id: 1
+   *          Description: "XD"
+   *          IsActived: true
+   *      500:
+   *        description: Internal Error
    */
-  async index () {
-    const roomtypes = await RoomType.query().where('IsActived', true).fetch()
+  //#endregion
+  async index() {
+    const roomtypes = await RoomType.query().where("IsActived", true).fetch();
 
-    return roomtypes
+    return roomtypes;
   }
 
+  //#region [Swagger: Method POST]
   /**
-   * Create/save a new roomtype.
-   * POST roomtypes
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+   * @swagger
+   * /roomtypes:
+   *  post:
+   *    tags:
+   *      - Room Type
+   *    summary: Create a room type
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: roomtype
+   *        in: body
+   *        required: false
+   *        type: string
+   *        example:
+   *          "{\n
+   *            \t\"Description\": \"XD\"
+   *          \n}"
+   *        schema:
+   *          $ref: '/roomtypes'
+   *    responses:
+   *      200:
+   *        description: Return a room type
+   *        example:
+   *          id: 1
+   *          Description: "XD"
+   *          IsActived: true
+   *      500:
+   *        description: Internal Error
    */
-  async store ({ request }) {
-    const data = request.only(['Description', 'IsActived'])
-    const roomtype = await RoomType.create(data)
+  //#endregion
+  async store({ request }) {
+    const data = request.only(["Description", "IsActived"]);
+    const roomtype = await RoomType.create(data);
 
-    return roomtype
+    return roomtype;
   }
 
+  //#region [Suagger: Method GET]
   /**
-   * Display a single roomtype.
-   * GET roomtypes/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @swagger
+   * /roomtypes/{id}:
+   *  get:
+   *    tags:
+   *      - Room Type
+   *    summary: Get a room type by Id
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        required: true
+   *        type: integer
+   *    responses:
+   *      200:
+   *        description: Return a room type
+   *        example:
+   *          id: 1
+   *          Description: "XD"
+   *          IsActived: true
+   *      500:
+   *        description: Internal Error
    */
-  async show ({ params }) {
-    const roomtype = await RoomType.findOrFail(params.id)
+  //#endregion
+  async show({ params }) {
+    const roomtype = await RoomType.findOrFail(params.id);
 
-    return roomtype
+    return roomtype;
   }
 
+  //#region [Swagger: Method PUT]
   /**
-   * Update roomtype details.
-   * PUT or PATCH roomtypes/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+   * @swagger
+   * /roomtypes/{id}:
+   *  put:
+   *    tags:
+   *      - Room Type
+   *    summary: Update a room type by Id
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        required: true
+   *        type: integer
+   *      - name: roomtype
+   *        in: body
+   *        required: false
+   *        type: string
+   *        example:
+   *          "{\n
+   *            \t\"Description\": \"XD\"
+   *          \n}"
+   *        schema:
+   *          $ref: '/roomtypes'
+   *    responses:
+   *      200:
+   *        description: Return a room type updated
+   *        example:
+   *          id: 1
+   *          Description: "XD"
+   *          IsActived: true
+   *      500:
+   *        description: Internal Error
    */
-  async update ({ params, request, response }) {
-    const roomtype = await RoomType.findOrFail(params.id)
-    const data = request.only(['Description', 'IsActived'])
+  //#endregion
+  async update({ params, request }) {
+    const roomtype = await RoomType.findOrFail(params.id);
+    const data = request.only(["Description", "IsActived"]);
 
-    roomtype.merge(data)
-    await roomtype.save()
+    roomtype.merge(data);
+    await roomtype.save();
 
-    return roomtype
+    return roomtype;
   }
 
+  //#region [Swagger: Method DELETE]
   /**
-   * Delete a roomtype with id.
-   * DELETE roomtypes/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+   * @swagger
+   * /roomtypes/{id}:
+   *  delete:
+   *    tags:
+   *      - Room Type
+   *    summary: Delete a room type by Id
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        required: true
+   *        type: integer
+   *    responses:
+   *      200:
+   *        description: Success
+   *      500:
+   *        description: Internal Error
    */
-  async destroy ({ params, request, response }) {
-    const roomtype = await RoomType.findOrFail(params.id)
+  //#endregion
+  async destroy({ params }) {
+    const roomtype = await RoomType.findOrFail(params.id);
 
-    roomtype.IsActived = false
+    roomtype.IsActived = false;
 
-    await roomtype.save()
+    await roomtype.save();
   }
 }
 
-module.exports = RoomTypeController
+module.exports = RoomTypeController;
