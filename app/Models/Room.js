999dec3594a3bbd1d44b6820c14faab4f9756bf9
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-19 21:59:12 -0300

    add swagger documentation in room model

--- app/Models/Room.js
+++ app/Models/Room.js
@@ -1,16 +1,39 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
+/**
+ *
+ * @swagger
+ *
+ * definitions:
+ *  Room:
+ *      type: object
+ *      properties:
+ *          id:
+ *              type: int
+ *          RoomTypeId:
+ *              type: int
+ *          ScreenId:
+ *              type: int
+ *          Name:
+ *              type: string
+ *          IsActived:
+ *              type: boolean
+ *      required:
+ *          - RoomTypeId
+ *          - ScreenId
+ *          - Name
+ */
 class Room extends Model {
-    roomtype(){
-        return this.belongsTo('App/Models/RoomType', 'RoomTypeId', 'id')
-    }
+  roomtype() {
+    return this.belongsTo("App/Models/RoomType", "RoomTypeId", "id");
+  }
 
-    screen(){
-        return this.belongsTo('App/Models/Screen', 'ScreenId', 'id')
-    }
+  screen() {
+    return this.belongsTo("App/Models/Screen", "ScreenId", "id");
+  }
 }
 
-module.exports = Room
+module.exports = Room;
