3fa7506ec05e2bc09d16c8cb8ae4923bec4fe333
Luiz Anthero Gama <luizanthero@icloud.com>
2020-04-19 22:14:39 -0300

    add swagger documentation in exhibition model

--- app/Models/Exhibition.js
+++ app/Models/Exhibition.js
@@ -1,28 +1,49 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
+/**
+ *
+ * @swagger
+ *
+ * definitions:
+ *  Exhibition:
+ *      type: object
+ *      properties:
+ *          id:
+ *              type: int
+ *          FilmId:
+ *              type: int
+ *          RoomId:
+ *              type: int
+ *          ScheduleId:
+ *              type: int
+ *      required:
+ *          - FilmId
+ *          - RoomId
+ *          - ScheduleId
+ */
 class Exhibition extends Model {
-    film(){
-        return this.belongsTo('App/Models/Film', 'FilmId', 'id')
-    }
+  film() {
+    return this.belongsTo("App/Models/Film", "FilmId", "id");
+  }
 
-    room(){
-        return this.belongsTo('App/Models/Room', 'RoomId', 'id')
-    }
+  room() {
+    return this.belongsTo("App/Models/Room", "RoomId", "id");
+  }
 
-    roomtype(){
-        return this.manyThrough('App/Models/Room', 'roomtype', 'id', 'id')
-    }
+  roomtype() {
+    return this.manyThrough("App/Models/Room", "roomtype", "id", "id");
+  }
 
-    screen(){
-        return this.manyThrough('App/Models/Room', 'screen', 'id', 'id')
-    }
+  screen() {
+    return this.manyThrough("App/Models/Room", "screen", "id", "id");
+  }
 
-    schedule(){
-        return this.belongsTo('App/Models/Schedule', 'ScheduleId', 'id')
-    }
+  schedule() {
+    return this.belongsTo("App/Models/Schedule", "ScheduleId", "id");
+  }
 }
 
-module.exports = Exhibition
+module.exports = Exhibition;
