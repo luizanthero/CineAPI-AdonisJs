8c49374be3038482e1e6b0fddcc437040b436645
Luiz Anthero Gama <luizantherogama@Luizs-MacBook-Air.local>
2020-05-24 15:04:06 -0300

    adding endpoints to User, store and authentication

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,95 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const User = use("App/Business/UserBusiness");
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  //#region [Swagger: Method POST]
+  /**
+   * @swagger
+   * /users:
+   *  post:
+   *    tags:
+   *      - User
+   *    summary: Create an user
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: user
+   *        in: body
+   *        required: false
+   *        type: string
+   *        example:
+   *          "{\n
+   *            \t\"Username\": \"teste\",
+   *            \n\t\"Email\": \"teste@teste.com\",
+   *            \n\t\"Password\": \"teste\"
+   *          \n}"
+   *        schema:
+   *          $ref: '/users'
+   *    responses:
+   *      200:
+   *        description: Return an user
+   *        example:
+   *          id: 1
+   *          Username: "teste"
+   *          Email: "teste@teste.com"
+   *          Password: "teste"
+   *      500:
+   *        description: Internal Error
+   */
+  //#endregion
+  async store({ request }) {
+    const user = await User.Create(request);
+
+    return user;
+  }
+
+  //#region [Swagger: Method POST]
+  /**
+   * @swagger
+   * /users/authentication:
+   *  post:
+   *    tags:
+   *      - User
+   *    summary: Get an User Token
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - name: user
+   *        in: body
+   *        required: false
+   *        type: string
+   *        example:
+   *          "{\n
+   *            \t\"Username\": \"teste\",
+   *            \n\t\"Password\": \"teste\"
+   *          \n}"
+   *        schema:
+   *          $ref: '/users/authentication'
+   *    responses:
+   *      200:
+   *        description: Return an user token
+   *        example:
+   *          id: 1
+   *          FilmId: 1
+   *          RoomId: 1
+   *          ScheduleId: 1
+   *      500:
+   *        description: Internal Error
+   */
+  //#endregion
+  async authentication({ request, auth }) {
+    const token = await User.GetToken(request, auth);
+
+    return token;
+  }
+}
+
+module.exports = UserController;
